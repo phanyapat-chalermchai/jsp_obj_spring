edf7670d45657be16cf513fdabe57d6acdee53f0
Phanyapat Chalermchai <81134953+phanyapat-chalermchai@users.noreply.github.com>
2024-06-02 18:59:30 +0700

    Initial commit

---
+++ src/main/java/com/jsp_obj/JspObjSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jsp_obj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspObjSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspObjSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp_obj/JspObjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp_obj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspObjSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
